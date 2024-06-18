21adb266427e9ec42f11975817186bcec5d23941
iMu21 <ik21.cse14@gmail.com>
2024-06-18 19:05:05 +0600

    api modified

--- forecasts/services/prediction_service.py
+++ forecasts/services/prediction_service.py
@@ -4,18 +4,24 @@ import pandas as pd
 def predict_monthly_payment_amount():
     file_path = "forecasts/data_analysis_reports/Sample_Data.xlsx"
     data = pd.read_excel(file_path)
-    last_payments = list(data['Payment Amount'])[-6:-1]
-    last_months = list(data['Month'])[-6:]
-    last_years = list(data['Year'])[-6:]
+    predict_month=5
+    took_month=predict_month*3
+    last_payments = list(data['Payment Amount'])[-took_month:-predict_month]
+    last_months = list(data['Month'])[-took_month:]
+    last_years = list(data['Year'])[-took_month:]
+    is_predicted_list = [False]*(took_month-predict_month)
     data = data.drop(columns=["Year","Month",'Payment Amount'])
-    last_month_data=data.iloc[[-1]]
     
     with open('forecasts/data_analysises/multioutput_gb_model.pkl','rb') as file:
         clf = pickle.load(file)
-        prediction = clf.predict(last_month_data)
-    last_payments.append(prediction[0][0])
+        
+        for i in range(1,predict_month+1):
+            month_data=data.iloc[[-i]]
+            prediction = clf.predict(month_data)
+            last_payments.append(prediction[0][0])
+            is_predicted_list.append(True)
     res=[]
     for i in range(len(last_payments)):
-        res.append({"payment":last_payments[i],"month":last_months[i],"year":last_years[i]})
+        res.append({"payment":last_payments[i],"month":last_months[i],"year":last_years[i],"is_predicted":is_predicted_list[i]})
 
     return res
